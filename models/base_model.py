132257aecd0f21754af49b0dff76ec5c3f04a5f2
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-08-30 12:46:48 +0100

    added more functionality to the project

---
+++ models/base_model.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+""" declaration of a class 'BaseModel' """
+
+
+class BaseModel:
+    """ initialization of instance attributes"""
+    def __init__(self, *args, **kwargs):
+        """ setting attributes for instance"""
+        if (len(kwargs) == 0):
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            l1 = []
+            i = 0
+            str1 = ""
+            str2 = ""
+            for k, v in kwargs.items():
+                if k != '__class__' and k != 'updated_at':
+                    if k != 'created_at':
+                        self.__dict__[k] = v
+
+            """ converting strings to datetime objof updated_at"""
+            str1 = kwargs['updated_at']
+
+            while i < len(str1):
+                if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
+                    l1.append(int(str2))
+                    str2 = ""
+                elif str1[i] == '.':
+                    l1.append(int(str2))
+                    str2 = ""
+                else:
+                    str2 += str1[i]
+                if i == len(str1) - 1:
+                    l1.append(int(str2))
+                i += 1
+
+            self.updated_at\
+                = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+
+            """ converting strings to datetime obj;of updated_at"""
+            i = 0
+            l1 = []
+            str2 = ""
+            str1 = kwargs['created_at']
+            while i < len(str1):
+                if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
+                    l1.append(int(str2))
+                    str2 = ""
+                elif str1[i] == '.':
+                    l1.append(int(str2))
+                    str2 = ""
+                else:
+                    str2 += str1[i]
+                if i == len(str1) - 1:
+                    l1.append(int(str2))
+                i += 1
+
+            self.created_at\
+                = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+
+    """ initialization of the str magic method """
+    def __str__(self):
+        """ str representation of object """
+        return "\
+[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    """ initilizing 'save' public method"""
+    def save(self):
+        """ updates the attr 'updated_at' """
+        self.updated_at = datetime.now()
+
+        """ returns a dictionary representation similar to
+__dict__ with some additions"""
+    def to_dict(self):
+        """ defining function to return dict"""
+        new_dict = self.__dict__
+        new_dict['__class__'] = type(self).__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
---
+++ tests/test_base/test_base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import unittest
+""" unittests for base_model"""
+
+
+class Base_test(unittest.TestCase):
+    """ tests for the unique id of object"""
+    def test_id(self):
+        """ beginning test for id"""
+        base1 = BaseModel()
+        self.assertEqual(base1.id, base1.id)
+    """ tests for the created_at object attr"""
+    def test_created_at(self):
+        """ beginning test for 'created_at'"""
+        base1 = BaseModel()
+        self.assertAlmostEqual(base1.created_at, base1.created_at)
+    """ tests for the updated_at  attr"""
+    def test_updated(self):
+        """ beginning the tests """
+        b1 = BaseModel()
+        self.assertAlmostEqual(b1.updated_at, b1.updated_at)
+
+    """ test for the 'to_dict' function"""
+    def test_to_dict(self):
+        """ beginning test for 'to_dict func"""
+        base1 = BaseModel()
+        diction = base1.to_dict()
+        base2 = BaseModel(**diction)
+        self.assertEqual(base1.id, base2.id)
+
+    """ test for the 'save' function"""
+    def test_save(self):
+        """ beginning test for 'save' """
+        base1 = BaseModel()
+        t = base1.updated_at
+        base1.save()
+        self.assertNotEqual(t, base1.updated_at)
