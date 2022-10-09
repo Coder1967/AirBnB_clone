5079fb4c2c812ddd4c2442df12f94ee41b58386f
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-09 13:50:46 +0000

    cleaning up

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,10 @@ class BaseModel:
     """ initialization of instance attributes"""
     def __init__(self, *args, **kwargs):
         """ setting attributes for instance"""
+        self.id = str(uuid4())
         if (len(kwargs) == 0):
-            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
         else:
             l1 = []
             i = 0
@@ -23,46 +22,52 @@ class BaseModel:
                 if k != '__class__' and k != 'updated_at':
                     if k != 'created_at':
                         self.__dict__[k] = v
+            try:
+                """ converting strings to datetime objof updated_at"""
+                str1 = kwargs['updated_at']
 
-            """ converting strings to datetime objof updated_at"""
-            str1 = kwargs['updated_at']
+                while i < len(str1):
+                    if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
+                        l1.append(int(str2))
+                        str2 = ""
+                    elif str1[i] == '.':
+                        l1.append(int(str2))
+                        str2 = ""
+                    else:
+                        str2 += str1[i]
+                    if i == len(str1) - 1:
+                        l1.append(int(str2))
+                    i += 1
 
-            while i < len(str1):
-                if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
-                    l1.append(int(str2))
-                    str2 = ""
-                elif str1[i] == '.':
-                    l1.append(int(str2))
-                    str2 = ""
-                else:
-                    str2 += str1[i]
-                if i == len(str1) - 1:
-                    l1.append(int(str2))
-                i += 1
+                self.updated_at\
+                    = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+            except KeyError:
+                self.updated_at = datetime.now()
 
-            self.updated_at\
-                = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+            try:
+                """ converting strings to datetime obj;of updated_at"""
+                i = 0
+                l1 = []
+                str2 = ""
+                str1 = kwargs['created_at']
+                while i < len(str1):
+                    if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
+                        l1.append(int(str2))
+                        str2 = ""
+                    elif str1[i] == '.':
+                        l1.append(int(str2))
+                        str2 = ""
+                    else:
+                        str2 += str1[i]
+                    if i == len(str1) - 1:
+                        l1.append(int(str2))
+                    i += 1
 
-            """ converting strings to datetime obj;of updated_at"""
-            i = 0
-            l1 = []
-            str2 = ""
-            str1 = kwargs['created_at']
-            while i < len(str1):
-                if str1[i] == '-' or str1[i] == ':' or str1[i] == 'T':
-                    l1.append(int(str2))
-                    str2 = ""
-                elif str1[i] == '.':
-                    l1.append(int(str2))
-                    str2 = ""
-                else:
-                    str2 += str1[i]
-                if i == len(str1) - 1:
-                    l1.append(int(str2))
-                i += 1
-
-            self.created_at\
-                = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+                self.created_at\
+                    = datetime(l1[0], l1[1], l1[2], l1[3], l1[4], l1[5], l1[6])
+            except KeyError:
+                self.created_at = datetime.now()
+            models.storage.new(self)
 
     """ initialization of the str magic method """
     def __str__(self):
